33997d717ff67690f0f0b73357c0d5bb7f0bd036
Rihards Veters <rihards@Rihardss-MacBook-Pro.local>
2018-07-25 01:38:50 +0300

    Adds login and register page. Adds authentification. Adds auth service. Adds token interceptor and protected routes

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,11 +1,15 @@
 import * as redux from 'redux';
-import { rentalReducer, selectedRentalReducer } from './rental-reducer';
 import thunk from 'redux-thunk';
+import { reducer as formReducer } from 'redux-form';
+import { rentalReducer, selectedRentalReducer } from './rental-reducer';
+import { authReducer } from './auth-reducer';
 
 export const init = () => {
     const reducer = redux.combineReducers({
         rentals: rentalReducer,
-        rental: selectedRentalReducer
+        rental: selectedRentalReducer,
+        form: formReducer,
+        auth: authReducer
     });
     const store = redux.createStore(reducer,redux.applyMiddleware(thunk));
 
